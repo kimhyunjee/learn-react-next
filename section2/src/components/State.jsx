c931cf256a3d626545da292be887de075018e9c9
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-10-04 23:57:40 +0900

    3-6. user input state change

--- section2/src/components/State.jsx
+++ section2/src/components/State.jsx
@@ -1,46 +1,41 @@
+import "./Body.css";
 import { useState } from "react";
 
-function Lightbulb({ light }) {
-  // console.log("lightbulb", light);
-  return (
-    <>
-      {light === "ON" ? (
-        <div style={{ backgroundColor: "orange" }}>ON</div>
-      ) : (
-        <div style={{ backgroundColor: "gray" }}>OFF</div>
-      )}
-    </>
-  );
-}
+export default function Body() {
+  const [state, setState] = useState({
+    name: "",
+    gender: "",
+    bio: "",
+  });
 
-function StaticLightbulb() {
-  return <div style={{ backgroundColor: "gray" }}>OFF</div>;
-}
-
-export default function State() {
-  /* const [어쩌고, setㅇ ㅓ쩌고] = useState("초기값") => js의 배열의 구조분해할당
-  [] 첫번째 인자는 state값, 두 번째는 state 상태변화 함수 / 그래서 첫번째거는 파란색 두번째거는 함수라 노란색
-  */
-  const [light, setLight] = useState("OFF");
+  const onChange = (e) => {
+    console.log(e.target.name + " : " + e.target.value);
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div className="body">
-      <Lightbulb light={light} />
-      <StaticLightbulb />
-      <button
-        onClick={() => {
-          setLight("ON");
-        }}
-      >
-        켜기
-      </button>
-      <button
-        onClick={() => {
-          setLight("OFF");
-        }}
-      >
-        끄기
-      </button>
+      <div>
+        <input
+          name="name"
+          placeholder="이름"
+          value={state.name}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <select name="gender" value={state.gender} onChange={onChange}>
+          <option value="">밝히지 않음</option>
+          <option value="male">남성</option>
+          <option value="female">여성</option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={state.bio} onChange={onChange} />
+      </div>
     </div>
   );
 }
