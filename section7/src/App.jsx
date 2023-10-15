27fb601cce3c3df9494d89696fd5f9969ae28d30
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-10-15 20:30:30 +0900

    7-2. prepare Implementation of features

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: "react 공부",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "기타치기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <>
       <div className="App">
