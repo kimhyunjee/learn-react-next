4755808c93f6561ee846c89c2bd999de6a3d6aa6
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-10-19 22:06:02 +0900

    8-1. Upgrade Todo list

--- section7/src/main.jsx
+++ section7/src/main.jsx
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import A from "./exam/a.jsx";
-import B from "./exam/B.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<B />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
