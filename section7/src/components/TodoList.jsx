3874c32a56360db949a2fc1ba6965b1372bcdb51
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-10-20 20:55:47 +0900

    9-1. useMemo- Prevention of re-acidification

--- section7/src/components/TodoList.jsx
+++ section7/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
@@ -18,9 +18,38 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
     );
   };
 
+  const getAnalyzedTodoData = () => {
+    console.log("todo 분석 함수 호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("todo 분석 함수 호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="TodoList">
       <h4>Todos</h4>
+      <div>
+        <div>전체 투두 : {totalCount}</div>
+        <div>완료 투두 : {doneCount}</div>
+        <div>미완 투두 : {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
