c6700673ab5114fe8f4e92e7156bc1bfed9abb18
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-10-28 18:54:25 +0900

    11-3. Move Page

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Country from "./pages/Country";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClick = () => {
+    nav("/search");
+  };
+
   return (
     <>
       <Routes>
@@ -14,6 +20,13 @@ function App() {
         <Route path="/country" element={<Country />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/search"}>Search</Link>
+        <Link to={"/country"}>county</Link>
+
+        <button onClick={onClick}> 서치 페이지로 이동</button>
+      </div>
     </>
   );
 }
