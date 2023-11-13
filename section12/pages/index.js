0c0f85e68f08ad9cc39e21cfce8e2a9af6759be4
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-11-13 23:57:08 +0900

    14-1 SSR

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,36 +1,21 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
-  const code = "KOR";
-  const router = useRouter();
+export default function Home({ name }) {
+  console.log("HOME");
 
-  const onClickButton = () => {
-    router.push({
-      pathname: "/country/[code]",
-      query: { code: code },
-    });
-  };
+  useEffect(() => {
+    console.log("HOME MOUNT");
+  }, []);
 
-  return (
-    <div>
-      Home Page
-      <div>
-        <button onClick={onClickButton}>search 페이지로 이동</button>
-      </div>
-      <div>
-        <Link href={"/search"}>Search Page로 이동</Link>
-      </div>
-      <div>
-        <Link
-          href={{
-            pathname: "/country/[code]",
-            query: { code: code },
-          }}
-        >
-          {code} 페이지로 이동
-        </Link>
-      </div>
-    </div>
-  );
+  return <div>{name}</div>;
 }
+
+export const getServerSideProps = async () => {
+  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+
+  return {
+    props: {
+      name: "KOREA",
+    },
+  };
+};
