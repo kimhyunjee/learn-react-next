afbd4da0c865b418182fc65a36175366acfa67ed
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-11-18 23:58:45 +0900

    14-4.1 console

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -11,14 +11,14 @@ export default function Home({ countries }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   /*
   SSR을 위해 서버측에서 페이지 컴포넌트(위의 Home컴포) 에게 전달할 데이터를 설정하는 함수
   이 컴포넌트는 반드시 객체를 반환해야 하며 안에 props라는 프로퍼티를 가지고 있어야 한다
   */
 
   const countries = await fetchCountries();
-
+  console.log("countries 데이터 불러옴");
   return {
     props: {
       countries,
